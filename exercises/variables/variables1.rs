275af37b587964226dcabc5e380d00f28e400b9f
pcarette <pierre.carette78@gmail.com>
2023-11-26 18:28:57 +0100

    core: functions excercises started

---
+++ exercises/variables/variables1.rs
@@ -0,0 +1,12 @@
+// variables1.rs
+//
+// Make me compile!
+//
+// Execute `rustlings hint variables1` or use the `hint` watch subcommand for a
+// hint.
+
+
+fn main() {
+    const x:i32 = 5;
+    println!("x has the value {}", x);
+}
